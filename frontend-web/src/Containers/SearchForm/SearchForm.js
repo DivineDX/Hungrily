2cc8481a3be1810023679a1b9a51e8fc5578c882
DivineDX <chia.dexun97@gmail.com>
2019-09-26 17:03:14 +0800

    Add Date Picker to SearchForm

--- frontend-web/src/Containers/SearchForm/SearchForm.js
+++ frontend-web/src/Containers/SearchForm/SearchForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Form, Input } from 'semantic-ui-react'
+import { Button, Form, Input, Icon } from 'semantic-ui-react'
 import { Formik } from "formik";
 import * as yup from "yup";
+import Flatpickr from 'react-flatpickr'
+import 'flatpickr/dist/themes/light.css'
 
 import CuisineOptions from '../../Data/CuisineOptions';
 import RestaurantOptions from '../../Data/RestaurantOptions';
@@ -29,7 +31,8 @@ const SearchForm = () => (
 
 
         validationSchema={yup.object().shape({
-            date: yup.string(),
+            date: yup.date("Invalid Date")
+                .min(new Date(), "Your cannot state a past date"),
             startTime: yup.string(),
             pax: yup.number().min(1).max(20),
             cuisine: yup.string(),
@@ -38,22 +41,22 @@ const SearchForm = () => (
         })}
 
         render={({ values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue }) => {
-            const handleDropdownChange = (e, {name, value}) => {
+            const handleDropdownChange = (e, { name, value }) => {
                 setFieldValue(name, value);
             };
 
             return (
                 <Form size='big' className='ba br4 b--light-silver pa3 appColor'>
                     <h3 className='f2 white '>Reserve your Restaurant</h3>
-                    <Form.Group widths='equal' className=''>
+                    <Form.Group widths='equal'>
                         <Form.Field>
-                            <Input //Input Date Picker
-                                placeholder='Date'
-                                name="date"
-                                value={values.date}
-                                onChange={handleChange}
+                            <Flatpickr
+                                options={{ minDate: 'today', maxDate: new Date().fp_incr(30) }}
+                                placeholder="Reservation Date"
+                                onChange={e => setFieldValue('date', e[0])}
                                 onBlur={handleBlur}
-                                icon='calendar'
+                                value={values.date}
+                                name="date"
                             />
                         </Form.Field>
 
@@ -84,7 +87,7 @@ const SearchForm = () => (
                         <Form.Select //Dropdown
                             placeholder='Any Cuisine'
                             name="cuisine"
-                            options={CuisineOptions} 
+                            options={CuisineOptions}
                             value={values.cuisine}
                             onChange={handleDropdownChange}
                         />
@@ -92,7 +95,7 @@ const SearchForm = () => (
                         <Form.Select //Dropdown
                             placeholder='Any Location'
                             name="location"
-                            options={LocationOptions} 
+                            options={LocationOptions}
                             value={values.location}
                             onChange={handleDropdownChange}
                         />
@@ -100,7 +103,7 @@ const SearchForm = () => (
                         <Form.Select //Dropdown
                             placeholder='Any Restaurant'
                             name="restaurant"
-                            options={RestaurantOptions} 
+                            options={RestaurantOptions}
                             value={values.restaurant}
                             onChange={handleDropdownChange}
                         />
