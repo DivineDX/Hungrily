8a05973cefc2b7a50c294d0a729f1fcba6407444
Sean <rootfourtytwo@gmail.com>
2019-10-14 22:32:56 +0800

    where clause for /search endpoint

--- backend-server/controllers/retrieve.js
+++ backend-server/controllers/retrieve.js
@@ -10,14 +10,25 @@ const getRestaurant = (req, res, db) => {
 
 const findRestaurants = (req, res, db) => {
     const {date, pax, cuisine, area, franchise} = req.body; //ignoring cuisine for now
-    console.log(req.body);
-    const restaurant = db.select().from("Restaurant")
+    //console.log(req.body);
+    const restaurant = db.select(
+        "Restaurant.Name",
+        "Restaurant.Address",
+        "Restaurant.Capacity",
+        "Restaurant.Area",
+        "Restaurant.Opening_hours",
+        "Restaurant.Closing_hours",
+        "Restaurant.FranchisorName",
+        "Restaurant.url"
+        ).from("Restaurant")
+        .innerJoin("Food", 'Restaurant.Name','Food.RestaurantName' )
         .where('Area', 'like', `%${area}%`)
         .andWhere('FranchisorName', 'like', `%${franchise}%`)
+        .andWhere('Cuisine', 'like', `%${cuisine}%`)
         //add cuisine
     .timeout(1000).then(
         result => {
-            console.log(result)
+            //console.log(result)
             res.status(200).json(result)
         }
     ).catch(err =>res.status(400).json('Unable to Retrieve'));
