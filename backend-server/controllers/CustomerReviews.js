e3868ea2f3910a818d6795a5796b6a6442782bd6
Sean <rootfourtytwo@gmail.com>
2019-11-09 01:46:02 +0800

    fixed rate

--- backend-server/controllers/CustomerReviews.js
+++ backend-server/controllers/CustomerReviews.js
@@ -44,21 +44,30 @@ const viewResReviews = (req, res, db) => {
  */
 const postRating = (req, res, db) => {
     //PRIMARY KEY (Customer_UserID, Restaurant_UserID, TableNum, Location, DateTime),
-    const {userID, location, resName, dateTime, table, rating,resUrl} = req.body;
+    const {userID, location, resid, dateTime, table, rating} = req.body;
     const sql = 
     `
     UPDATE
     Reservation
     SET
-    Reservation.rating = '${rating}'
+    rating = '${rating}'
     WHERE 
     Reservation.Customer_UserID = '${userID}'
-    AND Reservation.location ='${resUrl}'
-    AND Reservation.url ='${location}'
+    AND Reservation.location ='${location}'
+    AND Reservation.restaurant_userid ='${resid}'
+    AND Reservation.tablenum ='${table}'
+    AND Reservation.datetime ='${dateTime}'
     `
-    console.log(req.body)
-    
-    res.status(200).json('Success');
+    //console.log(req.body)
+    db.raw(sql)
+    .timeout(1000)
+    .then(result => {
+        //console.log(result)
+        res.status(200).json('Success')
+    }).catch(err => {
+        console.log(err)
+        res.status(400).json('Unable to Retrieve')});
+    //res.status(200).json('Success');
 }
 
 module.exports = {
