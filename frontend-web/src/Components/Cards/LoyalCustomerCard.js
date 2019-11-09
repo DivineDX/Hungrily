2ff8cd8cea12b1f57786fa9c9da53e3d670997f1
phaebaez <50545929+phaebaez@users.noreply.github.com>
2019-11-09 13:57:18 +0800

    basic most loyal customer

---
+++ frontend-web/src/Components/Cards/LoyalCustomerCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Icon, Card } from 'semantic-ui-react';
+import './LoyalCustomerCard.css'
+
+const LoyalCustomerCard = (loyalCustomerData) => {
+
+    const { userID, numBookings, percentBookings } = loyalCustomerData;
+
+    return (
+        <Card className="ui card" id = 'loyalCustomer'>
+            <Card.Content>
+                <Card.Header className = 'mb2 loyalHeaderText'>
+                Customer UserID: {userID}
+                </Card.Header>
+
+                <div>
+                    <Card.Meta>
+                        <Icon name='book' />
+                        <span className='ml1 loyalText'> Number of Bookings: {numBookings} </span>
+                    </Card.Meta>
+                    <Card.Meta>
+                        <Icon name='percent' />
+                        <span className='ml1 loyalText'> Percentage of Bookings: {percentBookings} </span>
+                    </Card.Meta>
+
+                </div>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default LoyalCustomerCard;
